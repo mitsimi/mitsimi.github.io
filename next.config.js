f2f5bcd3cb95a134fecabba3fa755030860dc81a
mitsimi <45365521+mitsimi@users.noreply.github.com>
2022-06-24 12:00:46 +0200

    tweaking files

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
+
+
+// next.config.js
+const isProd = process.env.NODE_ENV === 'production'
+
 const nextConfig = {
   reactStrictMode: true,
+  swcMinify: false,
+  assetPrefix: isProd ? '/mitsimi.github.io/' : ''
 }
 
 module.exports = nextConfig
