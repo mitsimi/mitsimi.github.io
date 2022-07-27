6acc1c5e35ea3b15882105e952176a4c1a8497bc
mitsimi <45365521+mitsimi@users.noreply.github.com>
2022-07-27 22:57:28 +0200

    prep

---
+++ pages/about.tsx
@@ -0,0 +1,10 @@
+import { NextPage } from "next";
+
+const About: NextPage = () => {
+    return (
+        <div>
+            <h1>About</h1>
+            <p>This is the about page</p>
+        </div>
+    );
+}
